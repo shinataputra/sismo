59e961c2ba37cab5aa4f5ec43e61ce677b7aac23
Shinataputra <shinata.putra@gmail.com>
2025-02-12 08:19:20 +0700

    Menambahkan fitur hapus

--- proseshapus.php
+++ proseshapus.php
@@ -1,3 +1,23 @@
 <?php
 
-include 'config.php';
+include 'koneksi.php';
+
+$id = $_GET['id'];
+
+// Query untuk mengambil data dari database
+$query = "SELECT * FROM pendaftaran WHERE id_pendaftaran = '$id'";
+$result = mysqli_query($conn, $query);
+
+if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    $nama = $row["nama"];
+    $alamat = $row["alamat"];
+    $nopol = $row["nopol"];
+    $type_motor = $row["type_motor"];
+    $paket_service = $row["paket_service"];
+    $keluhan = $row["keluhan"];
+} else {
+    echo "Tidak ada data pendaftaran.";
+}
+
+mysqli_close($conn);
